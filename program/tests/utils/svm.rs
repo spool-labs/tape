3148f87832cc5feac3f885384c11c586b6184b2b
NagaprasadVr <nagaprasadvr246@gmail.com>
2025-07-25 07:34:51 +0530

    fix github actions and add metadata sbf to elfs dir

--- program/tests/utils/svm.rs
+++ program/tests/utils/svm.rs
@@ -1,41 +1,24 @@
 use std::path::PathBuf;
-use std::str::FromStr;
 use std::error::Error;
-use solana_client::nonblocking::rpc_client::RpcClient;
-use steel::Pubkey;
 use tape_api::prelude::InstructionType;
 use solana_sdk::{signature::Keypair, signer::Signer, transaction::Transaction};
-use solana_sdk::bpf_loader_upgradeable::UpgradeableLoaderState;
 use solana_compute_budget::compute_budget::ComputeBudget;
 use litesvm::{types::{TransactionMetadata, TransactionResult}, LiteSVM};
 use pretty_hex::*;
 use bincode;
 
 pub fn program_bytes() -> Vec<u8> {
-    let mut so_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    so_path.push("../target/deploy/tape.so");
-    std::fs::read(so_path).unwrap()
+   // Fetch the tape program bytes from target
+   let mut so_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+   so_path.push("../target/deploy/tape.so");
+   std::fs::read(so_path).unwrap()
 }
 
-pub async fn metaplex_bytes() -> Result<Vec<u8>, Box<dyn Error>> {
-    let data_account = Pubkey::from_str("PwDiXFxQsGra4sFFTT8r1QWRMd4vfumiWC1jfWNfdYT")?;
-    let client = RpcClient::new("https://api.mainnet-beta.solana.com".to_string());
-
-    let account_data = client.get_account_data(&data_account).await?;
-    let state: UpgradeableLoaderState = bincode::deserialize(&account_data)?;
-
-    let elf_data = match state {
-        UpgradeableLoaderState::ProgramData { .. } => {
-            let metadata_size = UpgradeableLoaderState::size_of_programdata_metadata();
-            if account_data.len() < metadata_size {
-                return Err("Account data too short".into());
-            }
-            account_data[metadata_size..].to_vec()
-        }
-        _ => return Err("Not a ProgramData account".into()),
-    };
-
-    Ok(elf_data)
+pub fn metadata_bytes() -> Vec<u8> {
+    // Fetch the metadata program bytes from elfs/ dir before running the test
+    let mut so_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    so_path.push("tests/elfs/metadata.so");
+    std::fs::read(so_path).unwrap()
 }
 
 pub async fn setup_svm() -> Result<LiteSVM, Box<dyn Error>> {
@@ -44,9 +27,8 @@ pub async fn setup_svm() -> Result<LiteSVM, Box<dyn Error>> {
         ..Default::default()
     });
 
-    let metaplex_data = metaplex_bytes().await?;
 
-    svm.add_program(mpl_token_metadata::ID, &metaplex_data);
+    svm.add_program(mpl_token_metadata::ID, &metadata_bytes());
     svm.add_program(tape_api::ID, &program_bytes());
 
     Ok(svm)
