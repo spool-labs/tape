3c33680468aa2414dbf9fea7db03833b6ed2c170
Zelimir Fedoran <zelimir.fedoran@gmail.com>
2025-09-23 16:24:16 -0400

    adjust stake test balances

--- tape-program/tests/stake.rs
+++ tape-program/tests/stake.rs
@@ -8,7 +8,7 @@ use tape_api::prelude::*;
 #[test]
 fn test_stake() {
     let (mut svm, payer) = setup_environment();
-    initialize_program(&mut svm, &payer);
+    let treasury = initialize_program(&mut svm, &payer);
 
     let amount = TAPE::new(1000);
 
@@ -16,8 +16,10 @@ fn test_stake() {
     let stake_address = stake_with_node(&mut svm, &payer, node_address, amount);
 
     let stake_ata = get_ata_address(&MINT_ADDRESS, &stake_address);
-    let ata_balance = get_ata_balance(&svm, &stake_ata);
+    let stake_balance = get_ata_balance(&svm, &stake_ata);
+    let treasury_balance = get_ata_balance(&svm, &treasury);
 
-    assert_eq!(ata_balance, amount.as_u64());
+    assert_eq!(stake_balance, amount.as_u64());
+    assert_eq!(treasury_balance, MAX_SUPPLY - amount.as_u64());
 }
 
