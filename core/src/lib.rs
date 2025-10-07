7ecc779aeb2014138098343b99c2eab00b0bf925
Zelimir Fedoran <zelimir.fedoran@gmail.com>
2025-10-07 12:02:31 -0400

    nuked crypto types

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,13 +1,11 @@
 pub mod bft;
 pub mod bls;
 pub mod coin;
-pub mod hash;
 pub mod network;
 pub mod map;
 pub mod ring;
 pub mod system;
 pub mod shard;
-pub mod tree;
 pub mod types;
 mod macros;
 
@@ -15,12 +13,13 @@ pub mod prelude {
     pub use crate::bft::*;
     pub use crate::bls::*;
     pub use crate::coin::*;
-    pub use crate::hash::*;
     pub use crate::network::*;
     pub use crate::map::*;
     pub use crate::ring::*;
     pub use crate::system::*;
     pub use crate::shard::*;
-    pub use crate::tree::*;
     pub use crate::types::*;
+
+    pub use tape_crypto::*;
+    pub use tape_crypto::hash::Hash;
 }
--- core/src/tree/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod leaf;
-pub mod canopy;
-pub mod tree;
-pub mod utils;
-
-pub use canopy::*;
-pub use leaf::*;
-pub use tree::*;
-pub use utils::*;
