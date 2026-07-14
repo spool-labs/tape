b995dd2dd39f73a03d7e405c25da417d3a6ee3ae
Zelimir Fedoran <zelimir.fedoran@gmail.com>
2026-07-14 12:18:04 -0400

    added track list to overview page

--- network/gateway/src/http/handlers/object/mod.rs
+++ network/gateway/src/http/handlers/object/mod.rs
@@ -3,7 +3,10 @@ mod manifest;
 mod response;
 mod routes;
 
-pub(crate) use response::{ObjectResponseMetadata, object_headers};
+pub(crate) use response::ObjectResponseMetadata;
+pub(in crate::http::handlers) use response::{
+    range_header, ranged_object_headers, resolve_range,
+};
 pub(crate) use routes::{
     OBJECT_PATH, TRACK_BYTES_PATH, get_object, get_track_bytes, read_object_response,
 };
