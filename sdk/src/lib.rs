4ecaa6c4851dc3f15e44d646a415535dce3004c6
Zelimir Fedoran <zelimir.fedoran@gmail.com>
2026-07-29 21:21:35 -0400

    DO rollout test

--- sdk/src/lib.rs
+++ sdk/src/lib.rs
@@ -7,7 +7,6 @@ pub mod gateway;
 pub mod keys;
 pub mod metrics;
 pub mod object;
-pub mod read_options;
 pub mod staking;
 pub mod stream;
 pub mod tape;
@@ -18,5 +17,4 @@ pub mod write_options;
 
 pub use gateway::Gateway;
 pub use tapedrive::Tapedrive;
-pub use read_options::ReadOptions;
 pub use write_options::WriteOptions;
