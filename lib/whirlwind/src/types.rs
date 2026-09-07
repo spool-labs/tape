58765ba0939132557002d290b482b1773fa37228
crypt0miester <crypt0miester@gmail.com>
2026-09-07 09:07:43 +0400

    move whirlwind to its own repo

--- lib/whirlwind/src/types.rs
+++
@@ -1,9 +0,0 @@
-mod group_position;
-mod round;
-mod slot_count;
-
-pub use group_position::GroupPosition;
-pub use round::RoundNumber;
-pub use slot_count::SlotCount;
-
-pub use tape_core::types::{EpochNumber, SlotNumber, TrackNumber};
